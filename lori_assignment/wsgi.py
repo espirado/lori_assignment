0c5cf408d581f791b09437c47476237cff37ae2f
MacBook <macbook@ahmed.local>
2020-08-06 23:46:33 +0300

    CHORE add heroku deployment files

--- lori_assignment/wsgi.py
+++ lori_assignment/wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lori_assignment.settings.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lori_assignment.settings')
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
