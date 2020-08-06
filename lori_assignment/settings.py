15af3048d16a9b24d35902b0d7275af87cfb6873
MacBook <macbook@ahmed.local>
2020-08-06 23:22:29 +0300

    CHORE add heroku deployment files

--- lori_assignment/settings.py
+++ lori_assignment/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -124,9 +124,9 @@ REST_FRAMEWORK = {
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static-storage"),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static-storage"),
+# ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static-serve")
 
