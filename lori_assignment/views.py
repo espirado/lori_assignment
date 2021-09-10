c0d9e5c10fcdd94dad8bf919389c490f18df5b99
espirado <andiespirado@gmail.com>
2021-09-10 20:00:49 +0300

    upadated view

--- lori_assignment/views.py
+++ lori_assignment/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
-
+@csrf_exempt
 def index(request):
     return render(request, "index.html", {})
 
-
+@csrf_exempt
 def home(request):
     if request.user.is_authenticated:
         return render(request, "home.html", {"user":request.user, "id":request.user.id})
     else:
         return render(request, "index.html", {})
     
-
+@csrf_exempt
 def balance(request):
     if request.user.is_authenticated:
         return render(request, "balance.html", {"user":request.user, "id":request.user.id})
